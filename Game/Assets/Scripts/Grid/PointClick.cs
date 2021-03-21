ec59f348c471026d18634ed5920fdeca11c73a86
Lala-banners <lala.l.banno@hotmail.com>
2021-03-21 17:38:37 +1100

    Starting player grid

--- Game/Assets/Scripts/Grid/PointClick.cs
+++ Game/Assets/Scripts/Grid/PointClick.cs
@@ -2,16 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
-public class Test : MonoBehaviour
+public class PointClick : MonoBehaviour
 {
-    public LayerMask clickedOn;
-    private NavMeshAgent myAgent;
+    public LayerMask clickedOn; //to have a layer that differentiates on what can and cannot be clicked on
+    private NavMeshAgent myAgent; //Reference to nav mesh agent component on fake player
 
     // Start is called before the first frame update
     void Start()
     {
         myAgent = GetComponent<NavMeshAgent>();
-        
     }
 
     // Update is called once per frame
@@ -22,7 +21,7 @@ public class Test : MonoBehaviour
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hitInfo;
 
-            if(Physics.Raycast(ray, out hitInfo, 300, clickedOn))
+            if(Physics.Raycast(ray, out hitInfo, 100, clickedOn))
             {
                 myAgent.SetDestination(hitInfo.point);
             }
