e943a5e556eca51b79b0442530267280a50b1934
Lala-banners <lala.l.banno@hotmail.com>
2021-03-22 16:07:02 +1100

    Started grid script over

--- Game/Assets/Scripts/Grid/GridTest.cs
+++ Game/Assets/Scripts/Grid/GridTest.cs
@@ -4,32 +4,5 @@ using UnityEngine;
 
 public class GridTest : MonoBehaviour
 {
-    #region Grid Variables
-    [SerializeField] private float size = 1f;
-    #endregion
-
-    public Vector3 NearestGridPoint(Vector3 pos)
-    {
-        pos -= transform.position;
-
-        int countX = Mathf.RoundToInt(pos.x / size);
-        int countY = Mathf.RoundToInt(pos.y / size);
-        int countZ = Mathf.RoundToInt(pos.z / size);
-
-        Vector3 result = new Vector3(
-            countX * size,
-            countY * size, 
-            countZ * size);
-
-        result += transform.position;
-        return result;
-    }
-
-    // Implement this OnDrawGizmos if you want to draw gizmos that are also pickable and always drawn
-    private void OnDrawGizmos()
-    {
-        
-    }
-
 
 }
