9781e178b0394668dec74c354fc7736886fcfbe0
Lala-banners <lala.l.banno@hotmail.com>
2021-03-23 18:04:23 +1100

    Grid movement is good

--- Game/Assets/Scripts/Grid/GridTest.cs
+++ Game/Assets/Scripts/Grid/GridTest.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class GridTest : MonoBehaviour
 {
-    public static GridTest Instance = null;
     #region Instance
+    public static GridTest Instance = null;
     private void Awake()
     {
         if (Instance == null)
@@ -20,21 +20,28 @@ public class GridTest : MonoBehaviour
     }
     #endregion
 
-    
+    [SerializeField] private float size = 1f;
 
-    public void GetTiles(Transform destination, float max, float min = 0)
+    /// <summary>
+    /// Getting the number of points on the grid.
+    /// </summary>
+    /// <param name="pos">Position of the grid points</param>
+    /// <returns></returns>
+    public Vector3 GetNearestTiles(Vector3 pos)
     {
+        pos -= transform.position;
 
+        int xCount = Mathf.RoundToInt(pos.x / size);
+        int yCount = Mathf.RoundToInt(pos.y / size);
+        int zCount = Mathf.RoundToInt(pos.z / size);
+
+        Vector3 result = new Vector3(
+            (float)xCount * size,
+            (float)yCount * size,
+            (float)zCount * size);
+
+        result += transform.position;
+
+        return result;
     }
-
-    private void Start()
-    {
-
-    }
-
-    private void Update()
-    {
-        
-    }
-
 }
--- Game/Assets/Scripts/Grid/NEWPointClick.cs
+++ Game/Assets/Scripts/Grid/NEWPointClick.cs
@@ -4,28 +4,38 @@ using UnityEngine;
 
 public class NEWPointClick : MonoBehaviour
 {
-    //Tile Holder for managing each tile
-    [SerializeField] private Transform[] tiles;
     [SerializeField] private float moveSpeed = 5f;
-    public bool isMoving = false;
+    private GridTest grid;
+
+    private void Awake()
+    {
+        grid = FindObjectOfType<GridTest>();
+    }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        #region Grid Based Movement (atm only hover)
         Camera mainCamera = Camera.main;
-        Vector3 mousePos = Input.mousePosition;
-        Vector3 worldMousePos = mainCamera.ScreenToWorldPoint(mousePos);
-
-
-        /*if (Input.GetMouseButtonDown(0))
-        {*/
-        isMoving = true;
         RaycastHit hit;
-        Debug.Log(1 << 6);
+        //Debug.Log(1 << 6);
+
         if (Physics.Raycast(mainCamera.ScreenPointToRay(Input.mousePosition), out hit, 100000, 1 << 6))
         {
             transform.position = Vector3.MoveTowards(transform.position, hit.transform.position, moveSpeed * Time.deltaTime);
+            GetNearestTile(hit.point);
         }
-        //}
+        #endregion
     }
+
+    /// <summary>
+    /// This function will force the player to the squares of the grid
+    /// </summary>
+    /// <param name="nearPoint">Nearest point on the grid</param>
+    private void GetNearestTile(Vector3 nearPoint)
+    {
+        Vector3 finalPos = grid.GetNearestTiles(nearPoint);
+        transform.position = finalPos;
+    }
+    
 }
