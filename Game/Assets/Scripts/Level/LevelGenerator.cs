f55f9706e071d66709b8ea9735dcca25e0282314
Lala-banners <lala.l.banno@hotmail.com>
2021-03-26 17:00:42 +1100

    DANG GENERATOR

--- Game/Assets/Scripts/Level/LevelGenerator.cs
+++ Game/Assets/Scripts/Level/LevelGenerator.cs
@@ -4,32 +4,55 @@ using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
 {
-    public List<Transform> startingPositions; //Where to spawn the layout
+    public List<GameObject> room = new List<GameObject>(); //List of spawnable room layouts
+    public List<Transform> spawnPoints = new List<Transform>(); //List of spawn points
+    public Transform startingPosition; //Where to start spawning the layout
     private int direction;
     public float move;
     private float timeBetweenRoom;
-    public float startTime;
+    public float startTime = 1f;
 
-    public float minX;
-    public float maxX;
-    public float minY;
+    public float minX; //Minimum X top border
+    public float maxZ; //Maximum Z side border
+    public float minY; //Minimum X bottom border
+
+    private bool stopGeneration;
 
     private void Start()
     {
-        int randomStartPos = Random.Range(0, startingPositions.Count);
-        transform.position = startingPositions[randomStartPos].position;
-        //Spawn first room
-        Instantiate(SpawnLayout.Instance.room[0], transform.position, Quaternion.identity);
+        int random = Random.Range(1, room.Count); //Excluding starting room
+
+        int randomPos = Random.Range(0, spawnPoints.Count);
+        transform.position = spawnPoints[randomPos].position;
+        Instantiate(room[random], startingPosition.transform.position, Quaternion.identity);
+
+        direction = Random.Range(1, 6);
+    }
+
+    private void Update()
+    {
+        if(timeBetweenRoom <= 0 && stopGeneration == false)
+        {
+
+            timeBetweenRoom -= Time.deltaTime;
+            
+        }
+        else
+        {
+            MoveRoomLayout();
+            timeBetweenRoom = startTime;
+        }
     }
 
     public void MoveRoomLayout()
     {
-        #region Move room right
+        #region MOVE ROOM RIGHT
         if (direction == 1 || direction == 2)
         {
-            if(transform.position.x < maxX)
+            //If current z position is smaller than max Z (-65)
+            if (transform.position.z < maxZ)
             {
-                Vector3 newPosition = new Vector3(transform.position.x + move, transform.position.y);
+                Vector3 newPosition = new Vector3(transform.position.x - move, transform.position.z);
                 transform.position = newPosition;
             }
             else
@@ -37,14 +60,33 @@ public class LevelGenerator : MonoBehaviour
                 direction = 5;
             }
         }
-        #endregion
-
         #region Move room left
-
+        else if (direction == 3 || direction == 4)
+        {
+            if (transform.position.x > maxZ)
+            {
+                Vector3 newPosition = new Vector3(transform.position.x + move, transform.position.z);
+                transform.position = newPosition;
+            }
+            else
+            {
+                direction = 5;
+            }
+        }
+        else if(direction == 5)
+        {
+            if(transform.position.z < minY)
+            {
+                Vector3 newPosition = new Vector3(transform.position.x, transform.position.z + move);
+                transform.position = newPosition;
+            }
+            else
+            {
+                //Stop level generation
+                stopGeneration = true;
+            }
+        }
         #endregion
-
-        #region Move room up
-
         #endregion
     }
 }
