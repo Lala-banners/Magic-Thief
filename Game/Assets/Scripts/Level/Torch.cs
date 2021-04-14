139ac48b0fd14da11f4025630c0c8517747112cc
JoshMas <49138669+JoshMas@users.noreply.github.com>
2021-04-14 17:02:59 +1000

    Added script to torch

---
+++ Game/Assets/Scripts/Level/Torch.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Torch : MonoBehaviour
+{
+
+    public bool isLit = true;
+
+    private void OnMouseDown()
+    {
+        
+    }
+}
