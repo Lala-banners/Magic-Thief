93f191080251678ecc32b7777443932847b93fc2
JoshMas <49138669+JoshMas@users.noreply.github.com>
2021-04-28 11:14:06 +1000

    DOORS

---
+++ Game/Assets/Scripts/Level/Door.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    private Animator doorAnimator;
+
+    private void Awake()
+    {
+        doorAnimator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+            doorAnimator.SetTrigger("Open");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.CompareTag("Player"))
+            doorAnimator.SetTrigger("Open");
+    }
+}
